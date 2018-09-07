bc8d4efeafb840de6d34d6410fc2d40382086343
vlobos <suryvlobos@gmail.com>
2018-09-06 17:42:53 -0700

    Change Password type to Password

--- src/components/login.js
+++ src/components/login.js
@@ -15,7 +15,7 @@ class Login extends React.Component{
         <div className="credentials"> 
           <h3> LOGIN </h3>
             <input className="cred" type="text" name="company" placeholder="Organization" autoComplete="off" onChange={this.props.typeCredentials}/><br/>
-            <input className="cred" type="text" name="password" placeholder="Password" autoComplete="off" onChange={this.props.typeCredentials}/><br/>
+            <input className="cred" type="password" name="password" placeholder="Password" autoComplete="off" onChange={this.props.typeCredentials}/><br/>
             <button onClick={this.props.onSubmit}>Login</button><br/>
             <button onClick={this.props.signUp}>Sign Up</button> 
         </div>
